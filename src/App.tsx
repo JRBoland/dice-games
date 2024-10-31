f365c94a0cf76fd9324168fed8ab3747c4bb8e07
James Boland <102449868+JRBoland@users.noreply.github.com>
2024-10-31 12:30:56 +1100

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
-import { useEffect, useState } from 'react';
-import { io } from 'socket.io-client';
+import { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
+import './App.css'
 
-const socket = io(import.meta.env.VITE_SOCKET_SERVER_URL); // Configure WebSocket URL in .env file
+const socket = io(import.meta.env.VITE_SOCKET_SERVER_URL) // Configure WebSocket URL in .env file
 
 function App() {
-  const [gameResult, setGameResult] = useState<string | null>(null);
+  const [gameResult, setGameResult] = useState<string | null>(null)
 
   useEffect(() => {
     socket.on('game-result', (result) => {
-      setGameResult(result);
-    });
+      setGameResult(result)
+    })
 
     return () => {
-      socket.off('game-result');
-    };
-  }, []);
+      socket.off('game-result')
+    }
+  }, [])
 
   return (
-    <div>
-      <h1>Dice Roller</h1>
-      <input placeholder="Enter code" />
-      <button>Start a session</button>
-      <button>Join a session</button>
-      <button>Roll</button>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col gap-4 items-start my-4">
+        <h1 className="text-2xl font-bold">Dice Roller</h1>
+        <input className="border" placeholder="Enter code" />
+        <button>Start a session</button>
+        <button>Join a session</button>
+        <button>Roll</button>
+      </div>
       {gameResult && <p>Game Result: {gameResult}</p>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
